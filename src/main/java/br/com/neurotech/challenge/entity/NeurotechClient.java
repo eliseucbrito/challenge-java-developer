9d94ecc637a81d6f344cc292296ab75b00d43e51
eliseucbrito <ecb2@cin.ufpe.br>
2025-05-10 01:14:00 -0300

    feat(NeurotechClient): add table name

--- src/main/java/br/com/neurotech/challenge/entity/NeurotechClient.java
+++ src/main/java/br/com/neurotech/challenge/entity/NeurotechClient.java
@@ -1,5 +1,6 @@
 package br.com.neurotech.challenge.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -7,23 +8,22 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "neurotech_clients")
 public class NeurotechClient {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @NotBlank(message = "Name must not be null")
-    private String name;
-    @Min(value = 18, message = "The age must be at minimum 18 years")
-    private Integer age;
-    @Positive(message = "The income must be positive")
-    private Double income;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @NotBlank(message = "Name must not be null")
+  private String name;
+  @Min(value = 18, message = "The age must be at minimum 18 years")
+  private Integer age;
+  @Positive(message = "The income must be positive")
+  private Double income;
 }
