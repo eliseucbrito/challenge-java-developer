6218c85d479f26741742207bcf23cb6160d19e46
eliseucbrito <ecb2@cin.ufpe.br>
2025-05-09 21:36:53 -0300

    feat: add NeurotechClient

--- src/main/java/br/com/neurotech/challenge/entity/NeurotechClient.java
+++ src/main/java/br/com/neurotech/challenge/entity/NeurotechClient.java
@@ -1,12 +1,29 @@
 package br.com.neurotech.challenge.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class NeurotechClient {
-	
-	private String name;
-	private Integer age;
-	private Double income;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "Name must not be null")
+    private String name;
+    @Min(value = 18, message = "The age must be at minimum 18 years")
+    private Integer age;
+    @Positive(message = "The income must be positive")
+    private Double income;
 }
